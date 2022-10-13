d5ab68d14d92d682d6786bcb1818671b3742d340
whatwewant <tobewhatwewant@outlook.com>
2022-10-13 17:40:30 +0800

    feat: request config(yaml) support env

--- commands/request/request.go
+++ commands/request/request.go
@@ -3,11 +3,13 @@ package request
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/go-zoox/cli"
 	"github.com/go-zoox/core-utils/object"
+	"github.com/go-zoox/encoding/yaml"
 	"github.com/go-zoox/fetch"
-	"github.com/go-zoox/fs/type/yaml"
+	"github.com/go-zoox/fs"
 )
 
 // Create creates a request command.
@@ -35,9 +37,88 @@ func Create(app *cli.MultipleProgram) {
 				return fmt.Errorf("config is required")
 			}
 
-			cfg := fetch.Config{}
-			if err := yaml.Read(config, &cfg); err != nil {
+			cfgBytes, err := fs.ReadFile(config)
+			if err != nil {
 				return err
+			}
+
+			// // apply env
+			// // replace ${KEY} => env value
+			// reEnvNameWithBracket := regexp.MustCompile(`\$\{[^\s]+\}`)
+			// envdCfgBytes := reEnvNameWithBracket.ReplaceAllFunc(cfgBytes, func(matched []byte) []byte {
+			// 	envKey := string(matched[2 : len(matched)-1])
+
+			// 	// fmt.Println("key", envKey, " value:", os.Getenv(envKey))
+			// 	return []byte(os.Getenv(envKey))
+			// })
+
+			// // replace $KEY => env value
+			// reEnvNameWithoutBracket := regexp.MustCompile(`\$([^\s]+)`)
+			// envdCfgBytes = reEnvNameWithoutBracket.ReplaceAllFunc(envdCfgBytes, func(matched []byte) []byte {
+			// 	envKey := string(matched[1:])
+
+			// 	// fmt.Println("key", envKey, " value:", os.Getenv(envKey))
+			// 	return []byte(os.Getenv(envKey))
+			// })
+
+			cfg := fetch.Config{}
+			if err := yaml.Decode(cfgBytes, &cfg); err != nil {
+				return err
+			}
+
+			// apply env
+			if cfg.Headers != nil {
+				for k, s := range cfg.Headers {
+					if len(s) > 1 && s[0] == '$' {
+						if len(s) > 2 && s[1] == '{' {
+							cfg.Headers[k] = os.Getenv(s[2 : len(s)-1])
+						} else {
+							cfg.Headers[k] = os.Getenv(s[1:])
+						}
+					}
+				}
+			}
+
+			if cfg.Params != nil {
+				for k, s := range cfg.Params {
+					if len(s) > 1 && s[0] == '$' {
+						if len(s) > 2 && s[1] == '{' {
+							cfg.Params[k] = os.Getenv(s[2 : len(s)-1])
+						} else {
+							cfg.Params[k] = os.Getenv(s[1:])
+						}
+					}
+				}
+			}
+
+			if cfg.Query != nil {
+				for k, s := range cfg.Query {
+					if len(s) > 1 && s[0] == '$' {
+						if len(s) > 2 && s[1] == '{' {
+							cfg.Query[k] = os.Getenv(s[2 : len(s)-1])
+						} else {
+							cfg.Query[k] = os.Getenv(s[1:])
+						}
+					}
+				}
+			}
+
+			if cfg.Body != nil {
+				if body, ok := cfg.Body.(map[string]interface{}); ok {
+					for k, v := range body {
+						if s, ok := v.(string); ok {
+							if len(s) > 1 && s[0] == '$' {
+								if len(s) > 2 && s[1] == '{' {
+									body[k] = os.Getenv(s[2 : len(s)-1])
+								} else {
+									body[k] = os.Getenv(s[1:])
+								}
+							}
+						}
+					}
+
+					cfg.Body = body
+				}
 			}
 
 			response, err := fetch.New(&cfg).Execute()
